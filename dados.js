0e88c030dcbbd3e7cb20c18a72aa372d40c9c5f0
Samuel Henrique <samnrsk313@hotmail.com>
2024-09-06 12:08:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: 'Rebeca Andrade',
+        descricao: 'Rebeca Andrade, a joia da ginástica brasileira, conquistou o coração dos brasileiros com sua força, determinação e talento indiscutíveis. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, acumulando diversas medalhas em diferentes modalidades da ginástica artística. Nascida em Guarulhos, Rebeca representa um símbolo de superação e inspiração para jovens atletas, mostrando que com muito trabalho e dedicação é possível alcançar os mais altos patamares do esporte. Sua trajetória, marcada por momentos de glória e desafios, a tornou um verdadeiro ícone nacional.',
+        link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+        tags:'ginastica ginástica ouro'
+    },
+    {
+        titulo: 'Rayssa Leal',
+        descricao: 'Rayssa Leal, conhecida como "Fadinha", é uma skatista brasileira que conquistou o mundo com seu talento precoce e carisma. Aos poucos anos de idade, ela já colecionava títulos e patrocinadores, tornando-se um dos maiores nomes do skate mundial. Rayssa representou o Brasil nas Olimpíadas de Tóquio, onde se tornou a mais jovem medalhista olímpica da história do país. Sua história de superação, vindo de uma comunidade carente, inspira milhões de jovens a seguirem seus sonhos.',
+        link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+        tags:'skate fada sk8'
+    },
+    {
+        titulo: 'Beatriz Souza',
+        descricao: 'Beatriz Souza é uma ginasta brasileira que vem se destacando no cenário internacional. Com um estilo elegante e preciso, ela conquistou diversas medalhas em competições importantes e é considerada uma das maiores promessas da ginástica artística brasileira. Beatriz representa a nova geração de atletas brasileiras e tem como objetivo alcançar o topo do pódio nas próximas competições.',
+        link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza',
+        tags:'judô judo judoka'
+    },
+    {
+        titulo: 'Ana Marcela Cunha',
+        descricao: 'A nadadora brasileira é uma das maiores especialistas em maratonas aquáticas do mundo. Com diversas medalhas em campeonatos mundiais e pan-americanos, ela é um dos principais nomes da natação brasileira.',
+        link: 'https://pt.wikipedia.org/wiki/Ana_Marcela_Cunha',
+        tags:'nado natação natacao'
+    },
+    {
+        titulo: 'Alison dos Santos',
+        descricao: 'Piu, como é conhecido, é um velocista brasileiro que se destacou nas provas de 400 metros com barreiras. Sua explosão e técnica impecável o colocaram entre os principais nomes do atletismo mundial.',
+        link: 'https://pt.wikipedia.org/wiki/Alison_dos_Santos',
+        tags:'corrida rapido'
+    }
+]
---
+++ script.js
@@ -0,0 +1,41 @@
+function pesquisar() {
+    let section = document.getElementById('resultados-pesquisa');
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (campoPesquisa == ''){
+        section.innerHTML = '<p>Nada foi encontrado</p>' 
+    }
+    
+    let resultados = '';
+    let titulo = '';
+    let descricao = '';
+    let tags = '';
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado">
+                    <h2>
+                        ${dado.titulo}
+                    </h2>
+                    <p class="descricao-meta">
+                        ${dado.descricao}
+                    </p>
+                    <a href="${dado.link}" target="_blank">Mais informações.</a>
+            </div>
+            `
+        }
+    }
+
+    if (!resultados) {
+        resultados += `<p>Não existe resultados para "<span>${campoPesquisa}</span>"</p>`
+    }
+
+    section.innerHTML = resultados
+}
